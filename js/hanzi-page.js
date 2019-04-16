a8a50ff316e85bd836bfbf527daafeebb1d98599
hernanqs <hernan_quintero@live.com>
2019-04-16 13:07:09 -0300

    Fixed bug in display of stroke order image

--- js/hanzi-page.js
+++ js/hanzi-page.js
@@ -1,6 +1,6 @@
 function loadHanziPage(hanzi) {
 
-	mainSection = document.getElementById('main-section');
+	let mainSection = document.getElementById('main-section');
 
 	// Clear main section content
 	mainSection.innerHTML = '';
@@ -8,7 +8,7 @@ function loadHanziPage(hanzi) {
 
 	// Fill hanzi section
 
-	hanziSection = `
+	let hanziSection = `
 		<section class="hanzi-section">
 			<div class="simp-div">
 				<p class="simp" id="simp"></p>
@@ -54,7 +54,6 @@ function loadHanziPage(hanzi) {
 				</dl>
 			</div>
 			<div class="stroke-order-div" id="stroke-order-div">
-				<img src="" alt="" class="stroke-order-img" id="stroke-order-img">
 			</div>
 		</section>
 	`;
@@ -63,29 +62,39 @@ function loadHanziPage(hanzi) {
 
 
 	if (hanziDict[hanzi]) {
-		hanziMap = getHanziIdContentMap(hanzi, hanziDict);
+		let hanziMap = getHanziIdContentMap(hanzi, hanziDict);
 
 		populateHTMLById(hanziMap);
 	}
 
 	// Add stroke order image
-	strokeOrderImg = document.getElementById('stroke-order-img');
-	strokeOrderImg.setAttribute('src', 'data/stroke-orders/' + hanzi + '-bw.png');
-	strokeOrderImg.setAttribute('alt', 'Stroke order for ' + hanzi);
-
-	// If the image does not exist, add link for searching the image in Google Images
-	strokeOrderDiv = document.getElementById('stroke-order-div');
-
 	let img = new Image();
 	img.src = 'data/stroke-orders/' + hanzi + '-bw.png';
+	// If the image exists add image to the page
+	img.onload = function() {
+		let strokeOrderDiv = document.getElementById('stroke-order-div');
+		strokeOrderDiv.innerHTML = `<img
+			src="data/stroke-orders/${hanzi}-bw.png"
+			alt="Stroke order for ${hanzi}"
+			class="stroke-order-img"
+			id="stroke-order-img">
+		`;
+
+	}
+	// If the image does not exist, add link for searching the image in Google Images
 	img.onerror = function() {
-		strokeOrderDiv.innerHTML = '<a href="' +
-			'http://www.google.com/search?q=' + hanzi + ' stroke order&tbm=isch' +
-			'" target="_blank" id="search-stroke-order-link">Search the stroke order on Google Images</a>';
+		let strokeOrderDiv = document.getElementById('stroke-order-div');
+		strokeOrderDiv.innerHTML = `<a
+			href="http://www.google.com/search?q=${hanzi} stroke order&tbm=isch"
+			target="_blank"
+			id="search-stroke-order-link">
+				Search the stroke order on Google Images
+			</a>
+		`;
 	}
 
 	// Add link to Wiktionary entry on the hanzi
-	simpWiktionaryLink = document.getElementById('simp-w-link');
+	let simpWiktionaryLink = document.getElementById('simp-w-link');
 	simpWiktionaryLink.setAttribute('href', 'https://en.wiktionary.org/wiki/' + hanzi);
 
 
@@ -93,6 +102,6 @@ function loadHanziPage(hanzi) {
 	mainSection.innerHTML += makeCedictTable(
 		getCedictTableContent(cedictWordIndex[hanzi]),
 		'Cedict entries where the hanzi appears'
-		);
+	);
 	
 }
