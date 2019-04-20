45cf01bbb7b85d1b04dd43fc346a9008d9d74491
hernanqs <hernan_quintero@live.com>
2019-04-20 13:01:00 -0300

    Add radical page

---
+++ js/radical-page.js
@@ -0,0 +1,14 @@
+function loadRadicalPage(radical) {
+
+	mainSection = document.getElementById('main-section');
+
+	// Clear main section content
+	mainSection.innerHTML = '';
+
+	// Display table with hanzi where the radical appears
+	mainSection.innerHTML += makeHanziTable(
+		getHanziTableContent(radicalIndex[radical]),
+		'Hanzi in which this radical appears'
+		);
+
+}
